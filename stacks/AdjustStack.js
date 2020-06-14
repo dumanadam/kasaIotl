aff096412897ff3a5733c7ddbdb42a9e0ec0c292
Talha <tasezgi.n@gmail.com>
2020-06-15 00:33:07 +1000

    control buttons test

--- stacks/AdjustStack.js
+++ stacks/AdjustStack.js
@@ -4,9 +4,16 @@ import AdjustScreen from '../screens/AdjustScreen';
 
 const AdjustStack = props => {
   const AdjustStack = createStackNavigator();
+  console.log('adjust stack', props);
   return (
-    <AdjustStack.Navigator>
-      <AdjustStack.Screen name="Presets" component={AdjustScreen} />
+    <AdjustStack.Navigator
+      screenOptions={{headerStyle: {backgroundColor: 'papayawhip'}}}>
+      <AdjustStack.Screen
+        name={props.route.params.title}
+        component={AdjustScreen}
+        options={{name: 'Adjust'}}
+        initialParams={{title: 'asdasd', name: 'ERTGERG'}}
+      />
     </AdjustStack.Navigator>
   );
 };
