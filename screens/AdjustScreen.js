2a4ea63db21e0d1f0110730b05d18fb9b2adb29f
Talha <tasezgi.n@gmail.com>
2020-06-18 05:34:01 +1000

    colour wheel added api calls added

--- screens/AdjustScreen.js
+++ screens/AdjustScreen.js
@@ -1,13 +1,188 @@
 import * as React from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import {IotlStrings, IotlGlobals, AuthContext} from '../api/context';
+import AsyncStorage from '@react-native-community/async-storage';
+import {Secrets} from '../api/Secrets';
+import {useState} from 'react';
+import KasaControl from '../api/kasaKontrol';
+import {Header, Slider, Button, Text} from 'react-native-elements';
+import {ColorWheel} from '../api/ColorWheel';
+//import {ColorWheel} from 'react-native-color-wheel';
 
 const AdjustScreen = props => {
-  // console.log('adjust screen', props);
+  const {
+    signIn,
+    signUp,
+    signOut,
+    updateAuthObjTruth,
+    updateUserObjTruth,
+    getAppUserObj,
+    getAppAuthObj,
+  } = React.useContext(AuthContext);
+  const [count, setcount] = useState(0);
+  const [show, setshow] = useState(0);
+  const [userObj, setuserObj] = React.useState(getAppAuthObj('preset screen'));
+
+  React.useEffect(() => {
+    console.log('----------Settings ----------');
+    console.log('Settings authobj ', JSON.stringify(userObj));
+    setuserObj({...userObj, isEmailValid: true});
+    console.log('----------Settings Exit ----------');
+  }, []);
+
+  const update = () => {
+    setcount(count => count + 2);
+    route.params = {...route.params, asd: count => setcount(count)};
+  };
+
+  const tplinkLogin = async (sentLoginUserType, userObj) => {
+    const {login} = require('tplink-cloud-api');
+
+    let tplinkUser = '';
+    let tplinkPass = '';
+    let tplinkUUID = '';
+    let tplinkToken = '';
+    let tplinkDeviceList;
+
+    // log in to cloud, return a connected tplink object
+
+    tplinkUser = Secrets.demoUserName;
+    tplinkPass = Secrets.demoPassword;
+    tplinkUUID = Secrets.demoUUID;
+
+    /* const dogetToken = () => {
+      let _tplinkToken = tplink.getToken();
+      console.log('current auth token is', _tplinkToken);
+    }; */
+
+    const tplink = await login(tplinkUser, tplinkPass, tplinkUUID);
+    setshow(true);
+    // const colour = await tplink.getLB130('kasademots').setState(1, 90, 150, 80);
+
+    /* .catch(e => {
+      console.log('error', e);
+      sentAuthObj = {
+        ...sentAuthObj,
+        userNameError: 'User Credentials Error - TPLINK',
+        userPassError: "'User Credentials Error - TPLINK'",
+      };
+  
+      return sentAuthObj;
+    }); */
+    //console.log('current auth token is', tplink.getToken());
+    tplinkToken = tplink.getToken();
+    console.log('current auth token is', tplinkToken);
+
+    // get a list of raw json objects (must be invoked before .get* works)
+    tplinkDeviceList = await tplink.getDeviceList();
+    //console.log('tplinkDeviceList', tplinkDeviceList);
+
+    // find a device by alias:
+    let myPlug = tplink.getLB130(tplink.deviceList[0].alias);
+    console.log('alias is ', tplink.deviceList[0].alias);
+    await myPlug.setState(1, 50, 85, 85);
+
+    // or find by deviceId:
+    // let myPlug = tplink.getHS100("558185B7EC793602FB8802A0F002BA80CB96F401");
+    // console.log('myPlug:', myPlug);
+
+    // let response = await myPlug.powerOn();
+    // console.log('kasauth myplugpower response =' + JSON.stringify(response)); */
+
+    console.log('tplink return from  kasa imported library >>>>>>', tplink);
+    setuserObj({
+      tplink: tplink,
+      dl: tplinkDeviceList,
+      token: tplinkToken,
+      //MYPLUG: myPlug,
+    });
+    return tplink;
+
+    // find a device by alias:
+    //let myPlug = tplink.getHS100('My Smart Plug');
+    // or find by deviceId:
+    // let myPlug = tplink.getHS100("558185B7EC793602FB8802A0F002BA80CB96F401");
+    //console.log('myPlug:', myPlug);
+
+    //let response = await myPlug.powerOn();
+    //console.log("response=" + response );
+
+    /*  let response = await myPlug.toggle();
+      console.log('response=' + response);
+  
+      response = await myPlug.getSysInfo();
+      console.log('relay_state=' + response.relay_state);
+  
+      console.log(await myPlug.getRelayState()); */
+  };
+
   return (
-    <View style={{backgroundColor: 'black', flex: 1}}>
-      <Text style={{color: 'white', flex: 1}}>Adjust Screen</Text>
-    </View>
+    <ImageBackground
+      source={require('../assets/images/light_dc.jpg')}
+      style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Header
+          statusBarProps={{barStyle: 'default'}}
+          containerStyle={{
+            backgroundColor: 'transparent',
+
+            height: 40,
+            padding: 0,
+            margin: 0,
+
+            borderBottomWidth: 0,
+          }}
+          leftComponent={{icon: 'menu', color: '#F68F00'}}
+          centerComponent={{text: 'MY TITLE', style: {color: '#F68F00'}}}
+          rightComponent={{icon: 'home', color: '#fff'}}
+        />
+        <View style={{flex: 1}}>
+          {/*           <ColorWheel
+            initialColor="#ee0000"
+            onColorChange={color => console.log({color})}
+            onColorChangeComplete={color => onChange(color)}
+            style={{width: Dimensions.get('window').width}}
+            thumbStyle={{height: 30, width: 30, borderRadius: 30}}
+          /> */}
+          {/*   <ColorWheel
+            initialColor="#00ee00"
+            style={{marginLeft: 20, padding: 40, height: 200, width: 200}}
+          /> */}
+        </View>
+      </View>
+      <View style={{flex: 1}}>
+        <ColorWheel
+          initialColor="#ee0000"
+          onColorChange={color => console.log({color})}
+          style={{width: Dimensions.get('window').width}}
+          thumbStyle={{height: 150, width: 150, borderRadius: 60}}
+        />
+        <ColorWheel
+          initialColor="#000000"
+          style={{marginLeft: 20, padding: 40, height: 200, width: 200}}
+        />
+      </View>
+    </ImageBackground>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    alignItems: 'center',
+    marginTop: 150,
+  },
 
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    marginBottom: 20,
+    backgroundColor: 'black',
+  },
+});
 export default AdjustScreen;
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Text, View, StyleSheet, Button} from 'react-native';
 import {AuthContext} from '../api/context';
-import {tplinkLogin} from '../api/KasaAuthFunctions';
+import KasaControl from '../api/KasaControl';
 
 const SettingsScreen = ({navigation}) => {
   const {
@@ -19,6 +19,12 @@ const SettingsScreen = ({navigation}) => {
     ...getAppUserObj('from settings start'),
   });
 
+  const [kasaresult, setkasaresult] = React.useState('defaul');
+
+  const [kasadevices, setkasadevices] = React.useState(null);
+  const [kasax, setx] = React.useState(null);
+  const [kasapower, setkasapower] = React.useState(null);
+
   const [authObj, setAuthObj] = React.useState(getAppAuthObj('from settings'));
 
   React.useEffect(() => {
@@ -26,20 +32,106 @@ const SettingsScreen = ({navigation}) => {
     console.log('Settings authobj ', JSON.stringify(authObj));
 
     console.log('----------Settings Exit ----------');
-  }, []);
+  }, [authObj]);
 
   const updatepage = async () => {
-    //   console.log('update page', JSON.stringify(authObj));
-    // let tplinkDeviceList = await authObj.getDeviceList();
+    const kasa = new KasaControl();
+    let kasaSettings = {
+      mode: 'normal',
+      hue: 150,
+      saturation: 80,
+      color_temp: 2750,
+      brightness: 80,
+    };
 
-    //let result = authObj.tplinkObj.getToken();
-    /*    result = await tplinkLogin('demo', authObj);
-    result2 = result.tplinkObj.getToken(); */
-    console.log(
-      'tplinkobj from login page through state  >>>=',
-      JSON.stringify(authObj.tplinkObj),
-    );
-    //    console.log('RESULT2>>>=', JSON.stringify(result2));
+    await kasa.login('kasademo@talha.me', 'appliedproject2');
+    setkasaresult(kasa);
+    devices = await kasa.getDevices();
+    setkasadevices(devices);
+    // turn off first device
+    //tranition 0 - 10000 muilliseconds temp 0 - 7000 hue 0 - 360 brightness 0 - 100
+    const power = await kasa.power(devices[0].deviceId, true, 0, kasaSettings);
+    console.log('return USER in login from tplink', JSON.stringify(power));
+    setkasapower(power);
+    //mode, hue, saturation, color_temp, brightness
+
+    const info = await kasa.info('8012146C6839D9D29F024151439E90AE1C724BBA');
+    setkasaresult(info);
+    console.log('return USER in login from tplink', JSON.stringify(info));
+
+    //    console.log(power);
+
+    //send(authObj.auth);
+    //  const st = await allinone.hello();
+    //console.log(JSON.stringify(authObj));
+    //    const asd = new Chandu();
+
+    /*  const asd = new Chandu();
+    await asd.login('kasademo@talha.me', 'appliedproject2');
+    console.log(JSON.stringify(asd));
+    const ww = await asd
+      .send(JSON.stringify(authObj.kasaObj), {
+        'smartlife.iot.smartbulb.lightingservice': {
+          transition_light_state: {
+            on_off: 0,
+            transition_period: 0,
+          },
+        },
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(e => console.error(e)); */
+  };
+
+  const sethue = async theobject => {
+    const kasa = new KasaControl();
+    let kasaSettings = {
+      hue: 110,
+      saturation: 100,
+      brightness: 25,
+      color_temp: 0,
+    };
+
+    await kasa.login('kasademo@talha.me', 'appliedproject2');
+    setkasaresult(kasa);
+    // devices = await kasa.getDevices();
+
+    // turn off first device
+    //tranition 0 - 10000 muilliseconds temp 0 - 7000 hue 0 - 360 brightness 0 - 100
+    const power = await kasa.power(devices[0].deviceId, true, 0, kasaSettings);
+    console.log('return USER in login from tplink', JSON.stringify(power));
+    setkasapower(power);
+
+    //mode, hue, saturation, color_temp, brightness
+
+    const info = await kasa.info('8012146C6839D9D29F024151439E90AE1C724BBA');
+    setkasaresult(info);
+    //console.log('return USER in login from tplink', JSON.stringify(info));
+
+    //    console.log(power);
+
+    //send(authObj.auth);
+    //  const st = await allinone.hello();
+    //console.log(JSON.stringify(authObj));
+    //    const asd = new Chandu();
+
+    /*  const asd = new Chandu();
+    await asd.login('kasademo@talha.me', 'appliedproject2');
+    console.log(JSON.stringify(asd));
+    const ww = await asd
+      .send(JSON.stringify(authObj.kasaObj), {
+        'smartlife.iot.smartbulb.lightingservice': {
+          transition_light_state: {
+            on_off: 0,
+            transition_period: 0,
+          },
+        },
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(e => console.error(e)); */
   };
 
   const logOutSequence = () => {
@@ -56,20 +148,37 @@ const SettingsScreen = ({navigation}) => {
 
   return (
     <View>
-      <Text style={{fontSize: 9, color: 'red'}}>{JSON.stringify(authObj)}</Text>
+      <Text style={{fontSize: 9, color: 'red'}}>
+        {JSON.stringify(kasaresult.light_state)}
+      </Text>
       <Button
-        title="update page"
+        title="login kasacontrol"
         onPress={() => {
           updatepage();
         }}
       />
 
+      <Text style={{fontSize: 9, color: 'red'}}>
+        {JSON.stringify(kasadevices)}
+      </Text>
+      <Button
+        title="hue"
+        onPress={() => {
+          sethue();
+        }}
+      />
+      <Text style={{fontSize: 9, color: 'red'}}>
+        {JSON.stringify(kasapower)}
+      </Text>
       <Button
         title="Logout"
         onPress={() => {
           logOutSequence();
         }}
       />
+      <Text style={{fontSize: 9, color: 'red'}}>
+        {JSON.stringify(kasapower)}
+      </Text>
     </View>
   );
 };
